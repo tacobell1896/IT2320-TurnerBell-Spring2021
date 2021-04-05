924385b21f1847135aacffb2ef78d72da4cd1aad
Turner Bell <turner@tbell.us>
2021-04-05 11:20:48 -0400

    add determine winner function

--- MiniProject#Two/script.js
+++ MiniProject#Two/script.js
@@ -40,3 +40,25 @@ function computerTurn()
 	
 }
 
+function determineWinner()
+{
+	var winningPos = [[0,1,2],
+	[3,4,5],
+	[6,7,8],
+	[0,3,6],
+	[1,4,7],
+	[2,5,8],
+	[0,4,8],
+	[2,4,6]];
+
+	for(var i=0; i<winningPos.length; i++)	{
+		if(winningPos.values = "X")
+		{
+			document.write("Player is the winner");
+		}
+		else if(winningPos.values = "O")
+		{
+			document.write("Computer is the winner");
+		}
+	}
+}
